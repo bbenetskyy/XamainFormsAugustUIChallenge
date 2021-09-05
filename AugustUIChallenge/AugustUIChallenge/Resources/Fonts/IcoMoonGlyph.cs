e61fdc4069dd56d90109c8d2820bff5bbe3cb0c6
Bohdan Benetskyi <benetskyybogdan@gmail.com>
2021-09-05 18:43:58 +0200

    Add App Icons

---
+++ AugustUIChallenge/AugustUIChallenge/Resources/Fonts/IcoMoonGlyph.cs
@@ -0,0 +1,15 @@
+namespace AugustUIChallenge.Resources.Fonts
+{
+    public static class IcoMoonGlyph
+    {
+        public const string Map = "\ue002";
+        public const string Heart = "\ue003";
+        public const string Clock = "\ue004";
+        public const string Calendar = "\ue005";
+        public const string Search = "\ue006";
+        public const string Home = "\ue007";
+        public const string User = "\ue008";
+        public const string Mail = "\ue009";
+        public const string Github = "\ue00a";
+    }
+}
